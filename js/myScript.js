f7f96e495d37e9ca050c8122f7a2e74d2f44aca8
Nirmit Agrawal <nagraw18@asu.edu>
2022-12-24 17:59:47 -0700

    Adding Dynamic Titles V1.0

--- js/myScript.js
+++ js/myScript.js
@@ -1,3 +1,12 @@
 function title_spanning() {
+    const metas = document.getElementsByTagName('meta');
+
+    for (let i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute('name') === "titles") {
+            print(meta[i].getAttribute('content'));
+            return metas[i].getAttribute('content');
+        }
+    }
+
 
 }
