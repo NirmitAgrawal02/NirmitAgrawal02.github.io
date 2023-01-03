8b4ecf221450c27d49f6335f3b7dc84ce669375b
Nirmit Agrawal <nagraw18@asu.edu>
2023-01-02 18:19:08 -0700

    RWP V3.0

--- js/myScript.js
+++ js/myScript.js
@@ -1,10 +1,103 @@
-$(document).ready(function () {
-    var titles = new Typed('#titles', {
-        strings: ['Software Developer^2500', 'Computer Engineer^2500', 'Music Lover^2500', 'Chess Player^2500', 'Otaku'],
-        shuffle: true,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 100,
-        cursorChar: ' ',
-    })
-})
+(function () {
+
+    var doc = document.documentElement;
+    var w = window;
+
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curScroll;
+    var direction = 0;
+    var prevDirection = 0;
+
+    var header = document.getElementById('site-header');
+
+    var checkScroll = function () {
+
+        /*
+        ** Find the direction of scroll
+        ** 0 - initial, 1 - up, 2 - down
+        */
+
+        curScroll = w.scrollY || doc.scrollTop;
+        if (curScroll > prevScroll) {
+            //scrolled up
+            direction = 2;
+        }
+        else if (curScroll < prevScroll) {
+            //scrolled down
+            direction = 1;
+        }
+
+        if (direction !== prevDirection) {
+            toggleHeader(direction, curScroll);
+        }
+
+        prevScroll = curScroll;
+    };
+
+    var toggleHeader = function (direction, curScroll) {
+        if (direction === 2 && curScroll > 1000) {
+
+            //replace 52 with the height of your header in px
+
+            header.classList.add('hide');
+            prevDirection = direction;
+        }
+        else if (direction === 1) {
+            header.classList.remove('hide');
+            prevDirection = direction;
+        }
+    };
+
+    window.addEventListener('scroll', checkScroll);
+
+})();
+
+// // Hide Header on on scroll down
+// var didScroll;
+// var lastScrollTop = 0;
+// var delta = 5;
+// var navbarHeight = $('header').outerHeight();
+
+// $(window).scroll(function (event) {
+//     didScroll = true;
+// });
+
+// setInterval(function () {
+//     if (didScroll) {
+//         hasScrolled();
+//         didScroll = false;
+//     }
+// }, 250);
+
+// function hasScrolled() {
+//     var st = $(this).scrollTop();
+
+//     // Make sure they scroll more than delta
+//     if (Math.abs(lastScrollTop - st) <= delta)
+//         return;
+
+//     // If they scrolled down and are past the navbar, add class .nav-up.
+//     // This is necessary so you never see what is "behind" the navbar.
+//     if (st > lastScrollTop && st > navbarHeight) {
+//         // Scroll Down
+//         $('header').removeClass('nav-down').addClass('nav-up');
+//     } else {
+//         // Scroll Up
+//         if (st + $(window).height() < $(document).height()) {
+//             $('header').removeClass('nav-up').addClass('nav-down');
+//         }
+//     }
+
+//     lastScrollTop = st;
+// }
+
+// $(document).ready(function () {
+//     var titles = new Typed('#titles', {
+//         strings: ['Software Developer^2500', 'Computer Engineer^2500', 'Music Lover^2500', 'Chess Player^2500', 'Otaku'],
+//         shuffle: true,
+//         loop: true,
+//         typeSpeed: 100,
+//         backSpeed: 100,
+//         cursorChar: ' ',
+//     })
+// })
